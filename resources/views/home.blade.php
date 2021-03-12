01f7ed93e7413ea2ca66c2771246eea2e9f5f864
DiogoLima1 <62252469+DiogoLima1@users.noreply.github.com>
2021-03-12 18:31:04 -0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Blog</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <link href="css/style.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark scrolling-navbar">
+        <div class="container">
+
+          <a class="navbar-brand waves-effect" href="/">
+            <strong class="red-text">BLOG</strong>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            </ul>
+            <ul class="navbar-nav nav-flex-icons">
+              <li class="nav-item">
+                <a href="" class="nav-link border border-light rounded waves-effect">
+                  <i class="fas fa-user mr-2"></i>Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  </header>
+
+  <main class="mt-5 pt-5">
+    <div class="container">
+      <section class="card wow fadeIn" style="background-image: url(https://i.redd.it/8q0necj9qu141.jpg);">
+        <div class="card-body text-white text-center py-5 px-5 my-5">
+          <h1 class="mb-4">
+            <strong>Bem vindo ao Blog</strong>
+          </h1>
+          <p>
+            <strong>É um blog sobre diversos assuntos!</strong>
+          </p>
+        </div>
+      </section>
+      
+      <hr class="my-5">
+      <section class="text-center">
+        <div class="row mb-4 wow fadeIn">
+            <div class="col-lg-4 col-md-6 mb-4">      
+                <div class="card">      
+                  <div class="view overlay">
+                    <img src="https://digilandia.io/wp-content/uploads/2020/06/Sa%C3%BAde-mental-e-f%C3%ADsica-nas-empresas.jpg" class="card-img-top"
+                      alt="">
+                  </div>
+                  <div class="card-body">            
+                    <h4 class="card-title">Saúde Mental</h4>
+                    <p class="card-text">Mauris consequat dolor rhoncus, faucibus lorem sagittis, gravida velit. Sed aliquam non mi vitae tempus. Nulla sit amet elit dui. Etiam tempor placerat dolor, at sodales leo ullamcorper a...</p>
+                    <a href="/post" target="_blank"
+                      class="btn btn-danger btn-md">Ler mais...
+                    </a>
+                  </div>
+                </div>
+              </div>
+          
+          <div class="col-lg-4 col-md-6 mb-4">      
+            <div class="card">      
+              <div class="view overlay">
+                <img src="https://www.jornalbomdia.com.br/tb_noticias/30683/games.png" class="card-img-top"
+                  alt="">
+              </div>
+              <div class="card-body">            
+                <h4 class="card-title">Lista de melhores games de 2020</h4>
+                <p class="card-text">Morbi sed vestibulum ipsum. Vestibulum ac imperdiet mauris. Phasellus quis libero rhoncus, finibus odio quis, ultricies tellus. Aliquam iaculis vel odio vel sodales....</p>
+                <a href="#" target="_blank"
+                  class="btn btn-danger btn-md">Ler mais...
+                </a>
+              </div>
+            </div>
+          </div>
+         
+          <div class="col-lg-4 col-md-6 mb-4">
+           <div class="card">  
+              <div class="view overlay">
+                <img src="https://trii.com.br/wp-content/uploads/2019/04/2019-04-30-marketing-nas-redes-sociais-ou-blog-802x506.jpg" class="card-img-top" alt="">
+                  <div class="mask rgba-white-slight"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h4 class="card-title">Uso de redes sociais na pandemia</h4>   
+                <p class="card-text">Nulla facilisi. In vel mi vel risus vestibulum vehicula at cursus eros. Integer porttitor iaculis mattis. Vivamus tempor laoreet magna, at efficitur neque finibus vitae...</p>
+                  <a href="#" target="_blank"
+                  class="btn btn-danger btn-md">Ler mais...
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row mb-4 wow fadeIn">
+          <div class="col-lg-4 col-md-12 mb-4">
+            <div class="card">        
+              <div class="view overlay">
+                <img src="https://milvus.com.br/wp-content/uploads/tic_nas_empresas.jpg" class="card-img-top"
+                  alt="">
+              </div>
+              <div class="card-body">               
+                <h4 class="card-title">Avanço da tecnologia</h4>              
+                <p class="card-text"> Donec sit amet euismod lorem, non vehicula massa. Vivamus a mattis justo. Sed vel libero eget risus porta accumsan...</p>
+                <a href="#" target="_blank"
+                  class="btn btn-danger btn-md">Ler mais...
+                </a>
+              </div>
+            </div>
+          </div>
+         
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+              <div class="view overlay">
+                <img src="https://rollingstone.uol.com.br/media/_versions/sala-de-cinema-cinemark_divulgacao_widelg.jpg" class="card-img-top" alt="">
+              </div>
+              <div class="card-body">
+                <h4 class="card-title">Cinema vai acabar?</h4>
+                <p class="card-text">Ut ut semper metus. Morbi rhoncus aliquet sem in efficitur. Nulla nec egestas elit, sed lacinia libero. Mauris egestas turpis sit amet lorem pretium suscipit. Nullam eu tincidunt mauris...</p>
+                <a href="#" target="_blank"
+                class="btn btn-danger btn-md">Ler mais...
+              </a>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+              <div class="view overlay">
+                <img src="https://lh3.googleusercontent.com/MYWHyzqjk_ByAfgTAaXlWhuGYJJy37JuJg7Cj4PDO-t8C6hLIYMKa8I3R-NOsAZNd20wq5iAzA=w2880-h1619-l90-rj" class="card-img-top" alt="">
+              </div>
+              <div class="card-body">
+                <h4 class="card-title">Lista de músicas</h4>
+                <p class="card-text">Etiam et mauris bibendum, sagittis diam a, blandit sapien. Aenean dictum mauris et consectetur feugiat...</p>
+                <a href="#" target="_blank"
+                class="btn btn-danger btn-md">Ler mais...
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <nav class="d-flex justify-content-center wow fadeIn">
+          <ul class="pagination pg-red">
+            <li class="page-item disabled">
+              <a class="page-link" href="#" aria-label="voltar">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only">Voltar</span>
+              </a>
+            </li>
+            <li class="page-item active">
+              <a class="page-link" href="#">1</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">2</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">3</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">4</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">5</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#" aria-label="proximo">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Próximo</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </div>
+  </main>
+
+  <footer class="page-footer text-center font-small mdb-color black mt-4">
+    <div class="footer-copyright py-3">
+      © 2021 Blog
+    </div>
+  </footer>
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script type="text/javascript">
+    new WOW().init();
+  </script>
+</body>
+</html>
---
+++ resources/views/post.blade.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Blog</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <link href="css/style.min.css" rel="stylesheet">
+</head>
+
+<body class="grey lighten-3">
+  <header>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark scrolling-navbar">
+      <div class="container">
+        <a class="navbar-brand waves-effect" href="/">
+          <strong class="red-text">BLOG</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+          </ul>
+          <ul class="navbar-nav nav-flex-icons">
+            <li class="nav-item">
+              <a href="" class="nav-link border border-light rounded waves-effect"
+                target="_blank">
+                <i class="fas fa-user mr-2"></i>Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+ 
+  <main class="mt-5 pt-5">
+    <div class="container">
+      <section class="mt-4">
+        <div class="row">
+          <div class="col-md-8 mb-4">
+            <div class="card mb-4 wow fadeIn">
+              <img src="https://digilandia.io/wp-content/uploads/2020/06/Sa%C3%BAde-mental-e-f%C3%ADsica-nas-empresas.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="card mb-4 wow fadeIn">
+              <div class="card-body">
+                <p class="h5 my-4">Saúde Mental </p>
+                <blockquote class="blockquote">
+                  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                  <footer class="blockquote-footer">Someone famous in
+                    <cite title="Source Title">Source Title</cite>
+                  </footer>
+                </blockquote>
+                <p class="h5 my-4">Dicas de como manter uma boa saúde mental: </p>
+
+                <p> Praesent in quam augue. Praesent sit amet sapien eget arcu viverra tincidunt. Donec vel dictum velit. Quisque ultricies, nunc sed semper dignissim, sem arcu vehicula urna, nec varius neque ante non dolor. Curabitur efficitur mi metus, nec feugiat arcu tempus fermentum. Suspendisse tempus neque at felis tincidunt blandit.</p>
+
+                <p>Proin nisi ex, tincidunt ac neque in, feugiat convallis enim. Morbi ut nibh quis arcu dignissim sollicitudin bibendum eu orci. Vestibulum in libero tempor, semper massa eu, placerat justo. Ut vitae pulvinar nibh, eu tincidunt dui. Duis consequat molestie sapien. Nunc egestas tortor sem, non eleifend nibh euismod et. Suspendisse maximus posuere mattis. Proin efficitur massa leo, vel lobortis ante tincidunt ac. Vivamus a egestas tortor. Nunc malesuada, augue eu congue placerat, elit eros viverra lacus, at ultrices neque nunc vel tortor. Donec at ullamcorper ex, nec hendrerit lectus. Nunc ornare elit sit amet sem facilisis sollicitudin.</p>
+              </div>
+            </div>
+            <div class="card mb-4 wow fadeIn">
+              <div class="card-header font-weight-bold">
+                <span>Autor</span>
+              </div>
+              <div class="card-body">
+                <div class="media d-block d-md-flex mt-3">
+                  <img class="d-flex mb-3 mx-auto z-depth-1" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9c48b07e-431b-434e-8d85-1cedc3e2cef2/d70vm7t-3b72d3f3-9ec7-49f0-9718-920d9d4f74f7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWM0OGIwN2UtNDMxYi00MzRlLThkODUtMWNlZGMzZTJjZWYyXC9kNzB2bTd0LTNiNzJkM2YzLTllYzctNDlmMC05NzE4LTkyMGQ5ZDRmNzRmNy5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.S_yPWHw86KN21RYNjPmreQsqNBPhlFgbiP-qBFEk_Sk"
+                    alt="Generic placeholder image" style="width: 100px;">
+                  <div class="media-body text-center text-md-left ml-md-3 ml-0">
+                    <h5 class="mt-0 font-weight-bold">Matheus Costa
+                    </h5>
+                    Sed pellentesque, odio ac sodales maximus, turpis nunc vestibulum metus, ac volutpat mauris ipsum sit amet elit. Donec facilisis diam in metus malesuada blandit. Nulla id auctor sem. Maecenas ac dolor risus.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card card-comments mb-3 wow fadeIn">
+              <div class="card-header font-weight-bold">3 comentários</div>
+              <div class="card-body">
+                <div class="media d-block d-md-flex mt-4">
+                  <img class="d-flex mb-3 mx-auto " src="https://i.pinimg.com/originals/5c/9a/55/5c9a55dabaf3ec008e38354aa10d9a7a.jpg" alt="Generic placeholder image">
+                  <div class="media-body text-center text-md-left ml-md-3 ml-0">
+                    <h5 class="mt-0 font-weight-bold">Alice Lima
+                    </h5>
+                    Nam rhoncus, diam et vehicula fringilla, lorem nisi varius felis, ac posuere eros erat eu mauris. Donec molestie felis nec ex vestibulum semper. Aenean pellentesque eros in hendrerit scelerisque.
+                    <div class="media d-block d-md-flex mt-3">
+                      <img class="d-flex mb-3 mx-auto " src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9c48b07e-431b-434e-8d85-1cedc3e2cef2/d70vm7t-3b72d3f3-9ec7-49f0-9718-920d9d4f74f7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOWM0OGIwN2UtNDMxYi00MzRlLThkODUtMWNlZGMzZTJjZWYyXC9kNzB2bTd0LTNiNzJkM2YzLTllYzctNDlmMC05NzE4LTkyMGQ5ZDRmNzRmNy5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.S_yPWHw86KN21RYNjPmreQsqNBPhlFgbiP-qBFEk_Sk"
+                        alt="Generic placeholder image">
+                      <div class="media-body text-center text-md-left ml-md-3 ml-0">
+                        <h5 class="mt-0 font-weight-bold">Matheus Costa
+                          <a href="" class="pull-right">
+                            <i class="fas fa-reply red-text"></i>
+                          </a>
+                        </h5>
+                        Ut ac risus non lacus efficitur blandit. Nulla hendrerit tortor nec odio consectetur, in gravida ex rutrum. Integer non risus aliquet nisi eleifend egestas.
+                      </div>
+                    </div>
+                    <div class="media d-block d-md-flex mt-3">
+                      <img class="d-flex mb-3 mx-auto " src="https://64.media.tumblr.com/84365fe19039b5fd917d6d449ca86290/tumblr_op4lb5DPRe1qg6rkio1_1280.jpg"
+                        alt="Generic placeholder image">
+                      <div class="media-body text-center text-md-left ml-md-3 ml-0">
+                        <h5 class="mt-0 font-weight-bold">Felipe Silva
+                          <a href="" class="pull-right">
+                            <i class="fas fa-reply red-text"></i>
+                          </a>
+                        </h5>
+                        Pellentesque ut lacus nisl. In arcu libero, tincidunt gravida libero a, hendrerit aliquam augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                      </div>
+                    </div>
+                    <div class="form-group mt-4">
+                      <label for="quickReplyFormComment">Responda (É preciso está logado para comentar)</label>
+                      <textarea class="form-control" id="quickReplyFormComment" rows="5"></textarea>
+
+                      <div class="text-center">
+                        <button class="btn btn-danger btn-sm" type="submit">Enviar</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="media d-block d-md-flex mt-3">
+                  <img class="d-flex mb-3 mx-auto " src="https://64.media.tumblr.com/328878193efcc25fbd8d3e892e83d48e/tumblr_ovjxb9t0Ji1qla6e4o1_1280.gifv" alt="Generic placeholder image">
+                  <div class="media-body text-center text-md-left ml-md-3 ml-0">
+                    <h5 class="mt-0 font-weight-bold">Julieta Silva
+                    </h5>
+                    Cras luctus eros eget nibh maximus porttitor. Aenean vitae justo pharetra, tristique leo et, tempor mi. Pellentesque ut lacus nisl. In arcu libero, tincidunt gravida libero a, hendrerit aliquam augue.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card mb-3 wow fadeIn">
+              <div class="card-header font-weight-bold">Comente (É preciso está logado para comentar)</div>
+              <div class="card-body">
+                <form>
+                  <div class="form-group">
+                    <label for="reply">Seu comentário</label>
+                    <textarea class="form-control" id="reply" rows="5"></textarea>
+                  </div>
+                  <div class="text-center mt-4">
+                    <button class="btn btn-danger btn-md" type="submit">Enviar</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card mb-4 wow fadeIn">
+              <div class="card-header text-center">Faça seu cadastro!</div>
+              <div class="card-body">
+                <form>
+                  <label for="email" class="grey-text">Email</label>
+                  <input type="email" id="email" class="form-control">
+                  <br>
+                  <label for="name" class="grey-text">Nome</label>
+                  <input type="text" id="name" class="form-control">
+                  <br>
+                  <label for="password" class="grey-text">Senha</label>
+                  <input type="text" id="password" class="form-control">
+                  <div class="text-center mt-4">
+                    <button class="btn btn-danger btn-md" type="submit">Confirmar</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="card mb-4 wow fadeIn">
+              <div class="card-header">Postagens Recentes</div>
+              <div class="card-body">
+                <ul class="list-unstyled">
+                  <li class="media">
+                    <div class="media-body">
+                      <a href="">
+                        <h5 class="mt-0 mb-1 font-weight-bold red-text">Lista de melhores games 2020</h5>
+                      </a>
+                      Morbi sed vestibulum ipsum. Vestibulum ac imperdiet mauris. Phasellus quis libero rhoncus....
+                    </div>
+                  </li>
+                  <li class="media my-4">
+                    <div class="media-body">
+                      <a href="">
+                        <h5 class="mt-0 mb-1 font-weight-bold red-text">Uso de redes sociais na pandemia</h5>
+                      </a>
+                      Nulla facilisi. In vel mi vel risus vestibulum vehicula at cursus eros...
+                    </div>
+                  </li>
+                  <li class="media">
+                    <div class="media-body">
+                      <a href="">
+                        <h5 class="mt-0 mb-1 font-weight-bold red-text">Avanço da tecnologia</h5>
+                      </a>
+                      Donec sit amet euismod lorem, non vehicula massa. Vivamus a mattis justo...
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
+  <footer class="page-footer text-center font-small mdb-color black mt-4">
+    <div class="footer-copyright py-3">
+      © 2021 Blog 
+    </div>
+  </footer>
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script type="text/javascript">
+    new WOW().init();
+  </script>
+</body>
+</html>
+
